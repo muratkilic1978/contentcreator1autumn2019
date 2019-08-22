81a36fc9d68a4bc5a48ca6b7def2b43d8a8394cd
muratkilic1978 <muratkilic1978@gmail.com>
2019-08-22 06:57:35 +0200

    My first PHP code was uploaded
    My first PHP code was uploaded with simpel PHP commands

---
+++ day1/myfirstphp/index.php
@@ -0,0 +1,27 @@
+<?php
+echo "Murat Kilic";
+echo "I'm living in Aarhus";
+print "My sons name is Emre";
+
+echo "<h1> Murat </h1>";
+echo '<h1> Kilic </h1>';
+
+echo "<pre>5 + 5</pre>";
+
+
+$money1000 = 1000;
+$money100 = 100;
+
+$totalAmount = $money1000 + $money100;
+
+echo $totalAmount;
+
+
+
+$myFirstname = "Murat";
+$myLastname = "Kilic";
+$myMothersMothersLastnameIs = "Ekici";
+$mymothersmotherslastnameis = "Ekici";
+
+
+?>
